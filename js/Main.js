c79a48531f99b217f9922f689bf1ce70835c06c5
Zo0mM <srejica@gmail.com>
2020-07-11 03:36:12 +0200

    Added back to top button

--- js/Main.js
+++ js/Main.js
@@ -1,28 +1,66 @@
+///// FUNCTION TO REMOVE HOVER EFFECT ON TOUCH DEVICES /////
+
 function watchForHover() {
   // lastTouchTime is used for ignoring emulated mousemove events
   // that are fired after touchstart events. Since they're
   // indistinguishable from real events, we use the fact that they're
   // fired a few milliseconds after touchstart to filter them.
-  let lastTouchTime = 0
+  let lastTouchTime = 0;
 
   function enableHover() {
-    if (new Date() - lastTouchTime < 500) return
-    document.body.classList.add('hasHover')
+    if (new Date() - lastTouchTime < 500) return;
+    document.body.classList.add("hasHover");
   }
 
   function disableHover() {
-    document.body.classList.remove('hasHover')
+    document.body.classList.remove("hasHover");
   }
 
   function updateLastTouchTime() {
-    lastTouchTime = new Date()
+    lastTouchTime = new Date();
   }
 
-  document.addEventListener('touchstart', updateLastTouchTime, true)
-  document.addEventListener('touchstart', disableHover, true)
-  document.addEventListener('mousemove', enableHover, true)
+  document.addEventListener("touchstart", updateLastTouchTime, true);
+  document.addEventListener("touchstart", disableHover, true);
+  document.addEventListener("mousemove", enableHover, true);
 
-  enableHover()
+  enableHover();
 }
 
-watchForHover()
+watchForHover();
+
+
+
+
+///// FUNCTION TO PROVIDE BACK TO TOP BUTTON /////
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 200) {
+    // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "inline-flex";
+    }
+  } else {
+    // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function() {
+        backToTopButton.style.display = "none";
+      }, 200);
+      
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+  window.scrollTo(0, 0);
+}
